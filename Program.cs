85efbc5cc90dd12a9ef236086193d0d624a03dd9
Manuel Moya <manuelfrancisco111@gmail.com>
2022-01-24 11:16:10 -0300

    Proyecto inicial

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using WebAPIAutores;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var startup = new Startup(builder.Configuration);
+
+startup.ConfigureServices(builder.Services);
+
+
+var app = builder.Build();
+
+startup.Configure(app, app.Environment);
+
+// Configure the HTTP request pipeline.
+
+
+app.Run();
